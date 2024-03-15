5c1a2b33f44f4288864069bedeb1c2540c3b0200
Mazbauddin <mazbauddin2010@gmail.com>
2024-03-16 04:29:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+import Recipe from "./components/Recipe/Recipe";
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Recipe></Recipe>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-8xl text-white bg-purple-500">Recipe</h1>
+    </div>
+  );
+};
+
+export default Header;
